3cccb2a6062b98314448ffcb56f5fc49a21fff97
Ahsan-Ullah1871 <ahsanullahsunsbd@gmail.com>
2023-03-06 19:58:41 +0600

    - task finished

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const state = store.getState();
 const tagsArray = state.video?.video?.video?.tags;
 //  Dispatch fetchVideo thunk function
 store.dispatch(fetchVideo()).then(async (response) => {
+	// Fetching related videos
 	if (response?.payload?.tags?.length > 0) {
 		await store.dispatch(fetchRelatedVideo(response.payload.tags));
 	}
